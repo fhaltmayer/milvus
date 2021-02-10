e2dea570c2698bb4d05420b1c3aabb58ff2eddfe
fhaltmayer <filip.haltmayer@gmail.com>
2021-02-10 11:39:38 -0800

    cleanup

--- prepare_data.py
+++ prepare_data.py
@@ -4,13 +4,6 @@ import zipfile
 import pathlib
 import shutil
 
-def download():
-    # img_url = "https://drive.google.com/file/d/0B7EVK8r0v71pZjFTYXZWM3FlRnM/view?usp=sharing"
-    img_url = "https://drive.google.com/uc?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM"
-    # ident_url = "https://drive.google.com/file/d/1_ee_0u7vcNLOfNLegJRHmolfH5ICW-XS/view?usp=sharing"
-    output = 'celeba.zip'
-    ident_url = "https://drive.google.com/uc?id=1_ee_0u7vcNLOfNLegJRHmolfH5ICW-XS"
-
 def unzip():
     file = "./img_align_celeba.zip"
     with zipfile.ZipFile(file, 'r') as zip_ref:
@@ -33,9 +26,7 @@ def reorganize():
             shutil.copy(from_loc, to_loc) 
     shutil.rmtree("./img_align_celeba")
 if __name__ == '__main__':
-    # if len(sys.argv) != 3:
-    #     print("Incorret arguments: Arg1=ident_file, Arg2: Picture directory")
-    # else:
+
     print("Unzipping Data...")
     unzip()
     print("Reorganizing Data...")
